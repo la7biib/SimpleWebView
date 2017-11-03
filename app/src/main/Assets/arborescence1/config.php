fc6d35db9b6b018e72cec3fa8813d23aa6e64610
App Ben Dev <mohamedhabib.hammami@esprit.tn>
2017-11-03 19:57:09 +0100

    second commit

--- app/src/main/Assets/arborescence1/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$BDD_hote="localhost";
-$BDD_nmDB="cour-test2";
-$BDD_user="root";
-$BDD_pass="";
-$NAME_site=$_SERVER["SERVER_NAME"].dirname($_SERVER["PHP_SELF"]);
-$NAME_site1="http://www.systemics.fr/cours-test2/";
-
-?>
--- app/src/main/Assets/svg-to-png.js
+++
@@ -1,7 +0,0 @@
-var svg  = document.getElementById('graph'),
-    xml  = new XMLSerializer().serializeToString(svg),
-    data = "data:image/svg+xml;base64," + btoa(xml),
-    img  = new Image()
-
-img.setAttribute('src', data)
-document.body.appendChild(img)
